9f712c7f007a3069041cab7ac733e58174c56ac8
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2025-10-11 00:50:22 +0100

    Add additional plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,6 +31,48 @@ return {
       require("claude-code").setup()
     end
   },
+  {
+    "kiyoon/treesitter-indent-object.nvim",
+    keys = {
+      {
+        "ai",
+        function() require'treesitter_indent_object.textobj'.select_indent_outer() end,
+        mode = {"x", "o"},
+        desc = "Select context-aware indent (outer)",
+      },
+      {
+        "aI",
+        function() require'treesitter_indent_object.textobj'.select_indent_outer(true) end,
+        mode = {"x", "o"},
+        desc = "Select context-aware indent (outer, line-wise)",
+      },
+      {
+        "ii",
+        function() require'treesitter_indent_object.textobj'.select_indent_inner() end,
+        mode = {"x", "o"},
+        desc = "Select context-aware indent (inner, partial range)",
+      },
+      {
+        "iI",
+        function() require'treesitter_indent_object.textobj'.select_indent_inner(true, 'V') end,
+        mode = {"x", "o"},
+        desc = "Select context-aware indent (inner, entire range) in line-wise visual mode",
+      },
+    },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    version = "v2.20.8",  -- Use v2
+    event = "BufReadPost",
+    config = function()
+      vim.opt.list = true
+      require("indent_blankline").setup {
+        space_char_blankline = " ",
+        show_current_context = true,
+        show_current_context_start = true,
+      }
+    end,
+  },
 
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },

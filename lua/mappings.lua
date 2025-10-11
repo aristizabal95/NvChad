2244ebe2723f2c5a8941edeb7e4ac1ec4fb20185
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2025-10-11 13:31:20 +0100

    use smart splits plugin

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,15 +6,4 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
-map("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
-map("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
-map("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
-
--- Resize windows with Alt+hjkl
-map("n", "<M-h>", ":vertical resize -2<CR>", { desc = "Decrease window width", noremap = true, silent = true })
-map("n", "<M-l>", ":vertical resize +2<CR>", { desc = "Increase window width", noremap = true, silent = true })
-map("n", "<M-j>", ":resize +2<CR>", { desc = "Increase window height", noremap = true, silent = true })
-map("n", "<M-k>", ":resize -2<CR>", { desc = "Decrease window height", noremap = true, silent = true })
-
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")

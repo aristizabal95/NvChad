cb6d5eef5812436b5bfa0109b68d38e2becba941
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2025-10-17 20:42:40 +0100

    add overrides for vscode modern

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "material-darker",
+	theme = "vscode_dark",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
@@ -17,6 +17,44 @@ M.nvdash = {
   load_on_startup = true,
 }
 
+hl_override = {
+    Comment = {
+      italic = true,
+    },
+
+    ["@keyword.exception"] = {
+      link = "Conditional",
+    },
+
+    ["@variable.member"] = {
+      fg = "blue",
+    },
+  },
+
+  changed_themes = {
+    vscode_dark = {
+      base_16 = {
+        base00 = "#0F0F0F",
+        base01 = "#202020",
+        base03 = "#4D4D4D",
+        base04 = "#808080",
+        base08 = "#87CEFA",
+        base0B = "#CE9178",
+      },
+      base_30 = {
+        white = "#F4F4F4",
+        black = "#0F0F0F",
+        black2 = "#202020",
+        pink = "#F4C2C2",
+        green1 = "#00FF7F",
+        yellow = "#FFC40C",
+        teal = "#008080",
+        orange = "#FFA500",
+        pmenu_bg = "#4169E1",
+      },
+    },
+  },
+}
 -- M.nvdash = { load_on_startup = true }
 -- M.ui = {
 --       tabufline = {

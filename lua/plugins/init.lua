9cf74e464250b2463f59fcdee28cb7e73cbd4b2c
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2026-03-01 21:00:18 -0500

    Fix blankline indent

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,78 +1,77 @@
-
 return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-      opts = require "configs.conform",
+    opts = require "configs.conform",
   },
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
-      end,
+    end,
   },
   {
     "kylechui/nvim-surround",
     version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-      event = "VeryLazy",
+    event = "VeryLazy",
     config = function()
       require("nvim-surround").setup({
-          -- Configuration here, or leave empty to use defaults
-          })
+        -- Configuration here, or leave empty to use defaults
+      })
     end
   },
   {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
-      dependencies = { 'rafamadriz/friendly-snippets' },
+    dependencies = { 'rafamadriz/friendly-snippets' },
 
     -- use a release tag to download pre-built binaries
-      version = '1.*',
+    version = '1.*',
     -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-      -- build = 'cargo build --release',
+    -- build = 'cargo build --release',
     -- If you use nix, you can build from source using latest nightly rust with:
-      -- build = 'nix run .#build-plugin',
+    -- build = 'nix run .#build-plugin',
 
     ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
-      opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-          -- 'super-tab' for mappings similar to vscode (tab to accept)
-          -- 'enter' for enter to accept
-          -- 'none' for no mappings
-          --
-          -- All presets have the following mappings:
-          -- C-space: Open menu or open docs if already open
-          -- C-n/C-p or Up/Down: Select next/previous item
-          -- C-e: Hide menu
-          -- C-k: Toggle signature help (if signature.enabled = true)
-          --
-          -- See :h blink-cmp-config-keymap for defining your own keymap
-          keymap = { preset = 'default' },
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = { preset = 'default' },
 
-        appearance = {
-          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = 'mono'
-        },
-
-        -- (Default) Only show the documentation popup when manually triggered
-          completion = { documentation = { auto_show = false } },
-
-        -- Default list of enabled providers defined so that you can extend it
-          -- elsewhere in your config, without redefining it, due to `opts_extend`
-          sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
-          },
-
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-          -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-          --
-          -- See the fuzzy documentation for more information
-          fuzzy = { implementation = "prefer_rust_with_warning" }
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
       },
+
+      -- (Default) Only show the documentation popup when manually triggered
+      completion = { documentation = { auto_show = false } },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
     opts_extend = { "sources.default" }
   },
   {
@@ -137,19 +136,6 @@ return {
     },
   },
   {
-    "lukas-reineke/indent-blankline.nvim",
-    tag = "v2.20.8",  -- Use v2
-      event = "BufReadPost",
-    config = function()
-      vim.opt.list = true
-      require("indent_blankline").setup {
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-      }
-    end,
-  },
-  {
     'mrjones2014/smart-splits.nvim',
     config = function()
       -- Basic setup from README
@@ -172,7 +158,7 @@ return {
       vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
       vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
       vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
-      end,
+    end,
   },
   {
     "sindrets/winshift.nvim",
@@ -187,7 +173,20 @@ return {
     },
     config = function()
       require("winshift").setup({})
-      end,
+    end,
+  },
+  {
+    "gmr458/vscode_modern_theme.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("vscode_modern").setup({
+        cursorline = true,
+        transparent_background = false,
+        nvim_tree_darker = true,
+      })
+      vim.cmd.colorscheme("vscode_modern")
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter",
@@ -198,18 +197,31 @@ return {
       treesitter.setup()
       treesitter.install { 'python', 'java', 'c', 'lua', 'vim', 'vimdoc', 'query', 'html', 'yaml'}
 
-    vim.api.nvim_create_autocmd('FileType', {
+      vim.api.nvim_create_autocmd('FileType', {
         pattern = { 'python', 'java', 'c', 'lua', 'vim', 'vimdoc', 'query', 'html', 'yaml' },
         callback = function()
-        -- syntax highlighting, provided by Neovim
-        vim.treesitter.start()
-        -- folds, provided by Neovim (I don't like folds)
-        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        -- vim.wo.foldmethod = 'expr'
-        -- indentation, provided by nvim-treesitter
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          -- syntax highlighting, provided by Neovim
+          vim.treesitter.start()
+          -- folds, provided by Neovim (I don't like folds)
+          -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- vim.wo.foldmethod = 'expr'
+          -- indentation, provided by nvim-treesitter
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         end,
-        })
+      })
     end
-  }
+  },
+  -- Override NvChad's indent-blankline config to use v3 API correctly
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "User FilePost",
+    main = "ibl",
+    opts = {
+      indent = { char = "│" },
+      scope = {
+        show_start = false,
+        show_end = false,
+      },
+    },
+  },
 }
